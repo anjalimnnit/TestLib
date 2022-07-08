66d3420ecc75536dd344397cb0dbb790241ed4dc
anjalimnnit <anjalishah.mnnit@gmail.com>
2022-07-08 12:40:48 +0530

    Add project files.

---
+++ GarbageCollection/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace GarbageCollection
+{
+    public enum ResourceState
+    {
+        FREE, BUSY
+    }
+
+    public class Resource
+    {
+        public static readonly Resource Instance = new Resource();
+        private Resource() { }
+        public ResourceState State = ResourceState.FREE;
+
+    }
+    public class A
+    {
+
+        public static System.Threading.AutoResetEvent _handle = new System.Threading.AutoResetEvent(true);
+        public A()
+        {
+            lock (_handle)
+            {
+                if (Resource.Instance.State == ResourceState.FREE)
+                {
+                    Console.WriteLine($"Resource Owned By {System.Threading.Thread.CurrentThread.ManagedThreadId}");
+                }
+                else
+                {
+                    Console.WriteLine($"Resource Awaited By {System.Threading.Thread.CurrentThread.ManagedThreadId}");
+                    //wait
+                    _handle.WaitOne();
+                    Console.WriteLine($"Resource Owned By {System.Threading.Thread.CurrentThread.ManagedThreadId}");
+                }
+                Resource.Instance.State = ResourceState.BUSY;
+            }
+        }
+
+        public void UseResource()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Resource Used By thread {System.Threading.Thread.CurrentThread.ManagedThreadId} {i} times");
+                System.Threading.Thread.Sleep(1000);
+            }
+
+        }
+    }
+
+    class _Program
+    {
+        static void Main()
+        {
+            new System.Threading.Thread(Client).Start();
+            new System.Threading.Thread(Client).Start();
+        }
+        static void Client()
+        {
+            A obj = null;
+            obj = new A();
+            obj.UseResource();
+            obj = null;
+            GC.Collect();
+            Console.WriteLine("Successfully Garbage collected object A");
+        }
+    }
+}
---
+++ ThreadingConcepts/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadingConcepts
+
+{
+    class Program
+    {
+        private static AutoResetEvent event_1 = new AutoResetEvent(true);
+        private static AutoResetEvent event_2 = new AutoResetEvent(false);
+        static void Main(string[] args)
+        {
+
+            var t1 = Task.Factory.StartNew(() => PrintOddNumbers());
+            var t2 = Task.Factory.StartNew(() => PrintEvenNumbers());
+         
+            Task.WaitAll(t1, t2);
+            Console.WriteLine("End");
+            Console.ReadLine();
+        }
+
+        static void PrintOddNumbers()
+        {
+            int[] arr = new int[] { 1, 3, 5, 7, 9, 11, 13, 15 };
+            foreach (var item in arr)
+            {
+                event_1.WaitOne();
+                Console.WriteLine(item);
+                event_2.Set();
+            }
+        }
+
+        static void PrintEvenNumbers()
+        {
+            int[] arr = new int[] { 2, 4, 6, 8, 10, 12, 14 };
+            foreach (var item in arr)
+            {
+                event_2.WaitOne();
+                Console.WriteLine(item);
+                event_1.Set();
+            }
+        }
+    }
+}
